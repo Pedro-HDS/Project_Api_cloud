29f5d09b43fd43429b5d1228464d9b55ac1dce97
Pedro-HDS <pedro12897@gmail.com>
2025-05-15 15:33:57 -0300

    Initial commit

---
+++ src/main/java/pedro/io/Project_Api_cloud/Application.java
@@ -0,0 +1,13 @@
+package pedro.io.Project_Api_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/pedro/io/Project_Api_cloud/ProjectApiCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.io.Project_Api_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
